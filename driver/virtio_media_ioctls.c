6827c03d31589393cc7aaf15c884e8b39c63dcd3
Brian Daniels <briandaniels@google.com>
2026-05-18 09:48:35 -0400

    Make inclusion of control callbacks conditional based on linux version
    Tese control callbacks were removed in 6.15 by the following patch
    series:
    
    https://lore.kernel.org/r/20250223-queryctrl-v3-0-4292911cab6b@chromium.org/

--- driver/virtio_media_ioctls.c
+++ driver/virtio_media_ioctls.c
@@ -520,9 +520,11 @@ SIMPLE_WR_IOCTL(enum_framesizes, VIDIOC_ENUM_FRAMESIZES,
 		struct v4l2_frmsizeenum)
 SIMPLE_WR_IOCTL(enum_frameintervals, VIDIOC_ENUM_FRAMEINTERVALS,
 		struct v4l2_frmivalenum)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 15, 0)
 SIMPLE_WR_IOCTL(queryctrl, VIDIOC_QUERYCTRL, struct v4l2_queryctrl)
 SIMPLE_WR_IOCTL(g_ctrl, VIDIOC_G_CTRL, struct v4l2_control)
 SIMPLE_WR_IOCTL(s_ctrl, VIDIOC_S_CTRL, struct v4l2_control)
+#endif
 SIMPLE_WR_IOCTL(query_ext_ctrl, VIDIOC_QUERY_EXT_CTRL,
 		struct v4l2_query_ext_ctrl)
 SIMPLE_WR_IOCTL(s_dv_timings, VIDIOC_S_DV_TIMINGS, struct v4l2_dv_timings)
@@ -1207,10 +1209,12 @@ const struct v4l2_ioctl_ops virtio_media_ioctl_ops = {
 	.vidioc_s_output = virtio_media_s_output,
 
 	/* Control handling */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 15, 0)
 	.vidioc_queryctrl = virtio_media_queryctrl,
-	.vidioc_query_ext_ctrl = virtio_media_query_ext_ctrl,
 	.vidioc_g_ctrl = virtio_media_g_ctrl,
 	.vidioc_s_ctrl = virtio_media_s_ctrl,
+#endif
+	.vidioc_query_ext_ctrl = virtio_media_query_ext_ctrl,
 	.vidioc_g_ext_ctrls = virtio_media_g_ext_ctrls,
 	.vidioc_s_ext_ctrls = virtio_media_s_ext_ctrls,
 	.vidioc_try_ext_ctrls = virtio_media_try_ext_ctrls,
